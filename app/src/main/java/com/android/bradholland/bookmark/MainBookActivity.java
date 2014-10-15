08015e337b059ed7f0bd0c77da7f489f62f11ad5
bradholland84 <bholland.mail@gmail.com>
2014-10-15 15:20:23 -0700

    main activity listview actually functions! it shows book titles and descriptions for the currentuser's books.

--- app/src/main/java/com/android/bradholland/bookmark/MainBookActivity.java
+++ app/src/main/java/com/android/bradholland/bookmark/MainBookActivity.java
@@ -3,6 +3,7 @@ package com.android.bradholland.bookmark;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,8 +13,13 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseQueryAdapter;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 public class MainBookActivity extends Activity {
 
@@ -25,45 +31,49 @@ public class MainBookActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_book);
 
+        //get current user config and store
+        ParseUser currentUser = ParseUser.getCurrentUser();
 
-        ParseQueryAdapter.QueryFactory<Book> factory =
-         new ParseQueryAdapter.QueryFactory<Book>() {
+        //set up book query
+        ParseQuery<Book> query = ParseQuery.getQuery("Books");
+        query.whereEqualTo("user", currentUser);
+        query.findInBackground(new FindCallback<Book>() {
             @Override
-            public ParseQuery<Book> create() {
-                ParseQuery<Book> query = Book.getQuery();
-                query.include("title");
-                query.include("description");
-                //query.orderByDescending("date");
-                return query;
+            public void done(List<Book> booksList, ParseException e) {
+                Log.v("books", "retrieved" + booksList.size() + "books");
+            }
+        });
+
+        //constructs an adapter to apply queried books' data into the listview
+        final ParseQueryAdapter<Book> booksQueryAdapter = new ParseQueryAdapter<Book>(this, "Books") {
+            @Override
+            public View getItemView(Book book, View view, ViewGroup parent) {
+                if (view == null) {
+                    view = View.inflate(getContext(), R.layout.book_item, null);
+                }
+                TextView titleView = (TextView) view.findViewById(R.id.title_view);
+                TextView descriptionView = (TextView) view.findViewById(R.id.description_view);
+                titleView.setText(book.getTitle());
+                descriptionView.setText(book.getDescription());
+                return view;
             }
         };
 
-
-        booksQueryAdapter = new ParseQueryAdapter<Book>(this, factory) {
-        @Override
-        public View getItemView(Book book, View view, ViewGroup parent) {
-            if (view == null) {
-                view = View.inflate(getContext(), R.layout.book_item, null);
-            }
-            TextView titleView = (TextView) view.findViewById(R.id.title_view);
-            TextView descriptionView = (TextView) view.findViewById(R.id.description_view);
-            titleView.setText(book.getTitle());
-            descriptionView.setText(book.getDescription());
-            return view;
-        }
-    };
-
-        booksQueryAdapter.setAutoload(false);
+        booksQueryAdapter.setTextKey("title");
+        booksQueryAdapter.setTextKey("description");
 
         // attach query adapter to view
         ListView books_listview = (ListView) findViewById(R.id.books_listview);
         books_listview.setAdapter(booksQueryAdapter);
 
+
+        // set up click listener for book items
         books_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 final Book item = booksQueryAdapter.getItem(position);
                 selectedBookObjectId = item.getObjectId();
+                Log.v("BookID", selectedBookObjectId);
 
             }
         });
@@ -88,7 +98,7 @@ public class MainBookActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
-        doListQuery();
+      //  doListQuery();
 
     }
 
@@ -116,7 +126,7 @@ public class MainBookActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void doListQuery() {
-        booksQueryAdapter.loadObjects();
+  public void doListQuery() {
+       booksQueryAdapter.loadObjects();
     }
 }
