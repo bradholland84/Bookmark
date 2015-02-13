d3b0b398c14e2709e02392f3d69dba859a496f21
bradholland84 <bholland.mail@gmail.com>
2015-02-12 22:16:51 -0800

    added the current week and month counts to the graphs which use the accent color.

--- app/src/main/java/com/android/bradholland/bookmark/StatsFragment.java
+++ app/src/main/java/com/android/bradholland/bookmark/StatsFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.parse.GetCallback;
 import com.parse.ParseException;
@@ -23,9 +24,12 @@ import org.json.JSONException;
 public class StatsFragment extends Fragment {
 
     private BarChart weekBarChart;
+    private int currentMins;
     private BarChart allTitlesBarChart;
     private String title;
+    private TextView currentTitle;
     private JSONArray minutesHistory;
+
 
     public static StatsFragment newInstance(String bookId, int layoutInt) {
         StatsFragment f = new StatsFragment();
@@ -48,18 +52,26 @@ public class StatsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(getArguments().getInt("layoutInt"),container,false);
 
+        currentTitle = (TextView) v.findViewById((R.id.current_title));
         weekBarChart = (BarChart) v.findViewById(R.id.current_title_chart);
         allTitlesBarChart = (BarChart) v.findViewById(R.id.all_books_chart);
 
         ParseQuery<Book> query = ParseQuery.getQuery("Books");
-        //query.fromLocalDatastore();
         query.whereEqualTo("objectId", getArguments().getString("bookId"));
         query.getFirstInBackground(new GetCallback<Book>() {
             @Override
             public void done(Book book, ParseException e) {
                 title = book.getTitle();
-                minutesHistory = book.getWeekMinutesHistory();
-                populateChart(minutesHistory, weekBarChart);
+                currentTitle.setText(title);
+                if (getArguments().getInt("layoutInt") == R.layout.tab_weekly) {
+                    minutesHistory = book.getWeekMinutesHistory();
+                    currentMins = book.getWeeklyMinutes();
+                } else {
+                    minutesHistory = book.getMonthMinutesHistory();
+                    currentMins = book.getMonthlyMinutes();
+                }
+                populateChart(minutesHistory, weekBarChart, currentMins);
+
             }
         });
 
@@ -73,15 +85,18 @@ public class StatsFragment extends Fragment {
     }
 
     // Populates the chart view with data
-    private void populateChart(JSONArray array, BarChart chart) {
+    private void populateChart(JSONArray array, BarChart chart, int currentMins) {
         try {
             for (int i = 0; i < array.length(); i++) {
-                BarModel bm = new BarModel(array.getInt(i));
+                BarModel bm = new BarModel("Aug", array.getInt(i), 0xFF44B8B8);
                 chart.addBar(bm);
             }
         } catch (JSONException E) {
             E.printStackTrace();
         }
+        BarModel last = new BarModel(currentMins, 0xFFFFCA28);
+        chart.addBar(last);
+        chart.startAnimation();
     }
 
 }
