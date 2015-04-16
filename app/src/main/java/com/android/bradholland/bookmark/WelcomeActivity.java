cbb2babc92bffb8fbb3d5cc509967f8c5e051d20
bradholland84 <bholland.mail@gmail.com>
2015-04-15 23:46:15 -0700

    fixed the welcome screen, now allows for users to reset their password!

--- app/src/main/java/com/android/bradholland/bookmark/WelcomeActivity.java
+++ app/src/main/java/com/android/bradholland/bookmark/WelcomeActivity.java
@@ -14,9 +14,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.RequestPasswordResetCallback;
 import com.parse.SignUpCallback;
 import com.squareup.picasso.Picasso;
 
@@ -28,24 +30,30 @@ public class WelcomeActivity extends Activity {
     private EditText usernameEditText;
     private EditText passwordEditText;
     private EditText passwordAgainEditText;
+    private EditText emailEditText;
+    private boolean resetPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-
+        resetPassword = false;
         final Button registerButton = (Button) findViewById(R.id.signup_button);
         final Button loginButton = (Button) findViewById(R.id.login_button);
         usernameEditText = (EditText) findViewById(R.id.username);
         passwordEditText = (EditText) findViewById(R.id.password);
         passwordAgainEditText = (EditText) findViewById(R.id.passwordagain);
+        emailEditText = (EditText) findViewById(R.id.email);
+
 
         usernameEditText.setAlpha(0);
         passwordEditText.setAlpha(0);
         passwordAgainEditText.setAlpha(0);
+        emailEditText.setAlpha(0);
         usernameEditText.setEnabled(false);
         passwordEditText.setEnabled(false);
         passwordAgainEditText.setEnabled(false);
+        emailEditText.setEnabled(false);
 
         ImageView background = (ImageView) findViewById(R.id.iv_background);
         //ImageView logo = (ImageView) findViewById(R.id.logo);
@@ -57,10 +65,9 @@ public class WelcomeActivity extends Activity {
         loginButton.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
                 loginButton.setBackgroundColor(getResources().getColor(R.color.primary));
-                registerButton.setEnabled(false);
-                registerButton.setBackgroundColor(getResources().getColor(
-                        R.color.dim_foreground_disabled_material_light));
-                registerButton.setTextColor(getResources().getColor(R.color.black));
+                resetPassword = true;
+                registerButton.setBackgroundColor(getResources().getColor(R.color.accent));
+                registerButton.setText(getResources().getString(R.string.forgot_password));
 
                 usernameEditText.setAlpha(1);
                 passwordEditText.setAlpha(1);
@@ -92,34 +99,39 @@ public class WelcomeActivity extends Activity {
         registerButton.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
                 // Set up the signup form.
+                if (!resetPassword) {
+                    usernameEditText.setAlpha(1);
+                    passwordEditText.setAlpha(1);
+                    passwordAgainEditText.setAlpha(1);
+                    emailEditText.setAlpha(1);
+                    usernameEditText.setEnabled(true);
+                    passwordEditText.setEnabled(true);
+                    passwordAgainEditText.setEnabled(true);
+                    emailEditText.setEnabled(true);
+                    loginButton.setEnabled(false);
+                    loginButton.setAlpha(0);
 
-                usernameEditText.setAlpha(1);
-                passwordEditText.setAlpha(1);
-                passwordAgainEditText.setAlpha(1);
-                usernameEditText.setEnabled(true);
-                passwordEditText.setEnabled(true);
-                passwordAgainEditText.setEnabled(true);
-                loginButton.setEnabled(false);
-                loginButton.setAlpha(0);
-
-                passwordAgainEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-                    @Override
-                    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                        if (actionId == R.id.edittext_action_signup ||
-                                actionId == EditorInfo.IME_ACTION_UNSPECIFIED) {
-                            signup();
-                            return true;
+                    passwordAgainEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                        @Override
+                        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                            if (actionId == R.id.edittext_action_signup ||
+                                    actionId == EditorInfo.IME_ACTION_UNSPECIFIED) {
+                                signup();
+                                return true;
+                            }
+                            return false;
                         }
-                        return false;
-                    }
-                });
+                    });
 
-                // Set up the submit button click handler
-                registerButton.setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View view) {
-                        signup();
-                    }
-                });
+                    // Set up the submit button click handler
+                    registerButton.setOnClickListener(new View.OnClickListener() {
+                        public void onClick(View view) {
+                            signup();
+                        }
+                    });
+                } else {
+                    showPasswordResetDialog();
+                }
             }
         });
     }
@@ -182,6 +194,7 @@ public class WelcomeActivity extends Activity {
         String username = usernameEditText.getText().toString().trim();
         String password = passwordEditText.getText().toString().trim();
         String passwordAgain = passwordAgainEditText.getText().toString().trim();
+        String email = emailEditText.getText().toString().trim();
 
         // Validate the sign up data
         boolean validationError = false;
@@ -204,6 +217,13 @@ public class WelcomeActivity extends Activity {
             validationError = true;
             validationErrorMessage.append(getString(R.string.error_mismatched_passwords));
         }
+        if (email.length() == 0) {
+            if (validationError) {
+                validationErrorMessage.append(getString(R.string.error_email));
+            }
+            validationError = true;
+            validationErrorMessage.append(getString(R.string.error_no_email));
+        }
         validationErrorMessage.append(getString(R.string.error_end));
 
         // If there is a validation error, display the error
@@ -222,6 +242,7 @@ public class WelcomeActivity extends Activity {
         ParseUser user = new ParseUser();
         user.setUsername(username);
         user.setPassword(password);
+        user.setEmail(email);
 
 
         // Call the Parse signup method
@@ -241,4 +262,45 @@ public class WelcomeActivity extends Activity {
             }
         });
     }
+
+    public void showPasswordResetDialog() {
+        new MaterialDialog.Builder(this)
+                .title("Password reset")
+                .customView(R.layout.dialog_reset_password, false)
+                .positiveText("send")
+                .negativeText("cancel")
+                .negativeColorRes(R.color.primary)
+                .positiveColorRes(R.color.primary)
+                .callback(new MaterialDialog.ButtonCallback() {
+                    @Override
+                    public void onPositive(MaterialDialog dialog) {
+                        View v = dialog.getCustomView();
+                        EditText email = (EditText) v.findViewById(R.id.et_dialog_email);
+                        String s = email.getText().toString().trim();
+                        ParseUser.requestPasswordResetInBackground(s, new RequestPasswordResetCallback() {
+                            @Override
+                            public void done(ParseException e) {
+                                if (e == null) {
+                                    Toast.makeText(getBaseContext(), "Email sent", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+                    }
+                })
+                .show();
+    }
+
+    @Override
+    public void onPause() {
+        resetPassword = false;
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        resetPassword = false;
+        super.onDestroy();
+    }
+
 }
